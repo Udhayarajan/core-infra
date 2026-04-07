a356f368f80352afd916827583b509a3413451bf
udhayarajan <mudhayarajan.2013@gmail.com>
2026-04-07 16:23:43 +0530

    refactor: update Batcher to use slice for messages and improve concurrency handling

--- processor/batcher/batcher.go
+++ processor/batcher/batcher.go
@@ -1,107 +1,118 @@
 package batcher
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log/slog"
 	"os"
+	"sync"
 	"time"
 
 	"core-infra/common"
 )
 
 type Batcher struct {
-	messages      chan *common.CSV
+	messages      []*common.CSV
 	flushInterval time.Duration
-	limit         int
+	limit         int64
+	currentSize   int64
 	check         chan bool
 	batchCount    int
-	runID         string
+	rootPath      string
+	mu            sync.Mutex
 }
 
-func NewBatcher(batchLimit int, flushInterval time.Duration, runID string) *Batcher {
+func NewBatcher(batchLimit int64, flushInterval time.Duration, runID string) *Batcher {
 	return &Batcher{
-		messages:      make(chan *common.CSV, batchLimit),
+		messages:      make([]*common.CSV, 0, batchLimit),
 		flushInterval: flushInterval,
 		check:         make(chan bool),
 		limit:         batchLimit,
-		runID:         runID,
+		rootPath:      fmt.Sprintf("csv/%s", runID),
 	}
 }
 
 func (b *Batcher) Start(ctx context.Context) {
+	if err := os.MkdirAll(b.rootPath, 0o755); err != nil {
+		slog.Error("mkdir failed", slog.Any("err", err))
+		return
+	}
 	ticker := time.NewTicker(b.flushInterval)
 	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
+			b.flushBatch(b.getMessages())
 			return
 		case <-b.check:
-			if len(b.messages) == b.limit {
+			b.mu.Lock()
+			shouldFlush := b.currentSize >= b.limit
+			b.mu.Unlock()
+			if shouldFlush {
 				slog.Info("batcher flush limit exceeded")
-				b.flush()
+				b.flushBatch(b.getMessages())
 				ticker.Reset(b.flushInterval)
 			}
 		case <-ticker.C:
 			slog.Info("batcher flush interval")
-			b.flush()
+			b.flushBatch(b.getMessages())
 			ticker.Reset(b.flushInterval)
 		}
 	}
 }
 
 func (b *Batcher) AddMessage(msg *common.CSV) {
-	b.messages <- msg
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.messages = append(b.messages, msg)
+	b.currentSize += int64(len(msg.Bytes()))
 	select {
 	case b.check <- true:
 	default:
 	}
 }
 
-func (b *Batcher) flush() {
-	if len(b.messages) == 0 {
+func (b *Batcher) flushBatch(messages []*common.CSV) {
+	if len(messages) == 0 {
 		return
 	}
 
-	if err := os.MkdirAll(fmt.Sprintf("csv/%s", b.runID), 0o755); err != nil {
-		slog.Error("mkdir failed", slog.Any("err", err))
-		return
-	}
+	b.batchCount++
+	path := fmt.Sprintf("%s/batch_%d.csv", b.rootPath, b.batchCount)
 
-	path := fmt.Sprintf("csv/%s/batch_%d.csv", b.runID, b.batchCount)
-	csvBatchFile, err := os.Create(path)
+	f, err := os.Create(path)
 	if err != nil {
 		slog.Error("open batch file failed", slog.Any("err", err), slog.String("path", path))
 		return
 	}
+	defer f.Close()
 
-	slog.Info("created batch file", slog.String("path", path))
+	w := bufio.NewWriter(f)
 
-	defer func() {
-		if err := csvBatchFile.Close(); err != nil {
-			slog.Error("close batch file failed", slog.Any("err", err))
+	written := 0
+	for _, msg := range messages {
+		_, err := w.Write(msg.Bytes())
+		if err != nil {
+			slog.Error("write batch failed", slog.Any("err", err))
 		}
-	}()
-
-	defer func() {
-		b.batchCount++
-	}()
-
-	for {
-		select {
-		case msg, ok := <-b.messages:
-			if !ok {
-				return
-			}
-			if _, err := csvBatchFile.Write(append(msg.Bytes(), '\n')); err != nil {
-				slog.Error(err.Error())
-			}
-		default:
-			return
-		}
+		written++
 	}
+
+	if err := w.Flush(); err != nil {
+		slog.Error("flush batch file failed", slog.Any("err", err))
+	}
+	slog.Info("flushed batch", slog.String("path", path), slog.Int("messages", written))
 }
 
-func (b *Batcher) Limit() (int, time.Duration) {
+func (b *Batcher) Limit() (int64, time.Duration) {
 	return b.limit, b.flushInterval
+}
+
+func (b *Batcher) getMessages() []*common.CSV {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	old := b.messages
+	b.messages = make([]*common.CSV, 0, b.limit)
+	return old
 }
