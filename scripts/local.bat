7839ce82d236a5fe4ff4b5e10615c7009a5b8a94
udhayarajan <mudhayarajan.2013@gmail.com>
2026-04-10 04:09:46 +0530

    feat(local): update batch size for processor and adjust generator command

--- scripts/local.bat
+++ scripts/local.bat
@@ -1,4 +1,4 @@
 @echo off
-start "processor" cmd /k go run ./processor/main.go -max=11_000_000
-start "generator" cmd /k go run ./generator/main.go -total=11_000_000
+start "processor" cmd /k go run ./processor/main.go -max=11_000_000 -batch=100_000_000
+start "generator" cmd /k go run ./generator/main.go -max=11_000_000
 start "validator" cmd /k go run ./validator/main.go
